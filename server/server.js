f31c1215306621de593113c7d31aa58bceb9765f
bigredtiger <simon@bigredtiger.com>
2018-05-16 13:47:32 -0400

    Updated x-domain

--- server/server.js
+++ server/server.js
@@ -12,8 +12,10 @@ function allowCrossDomain(req, res, next) {
     next();
 }
 
-app.use(allowCrossDomain);
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true });
+app.use(allowCrossDomain);
+
 app.listen(3000, function() {
     console.log('Server running at http://localhost:3000/');
 });
